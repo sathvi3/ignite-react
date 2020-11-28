4736514a524a1a8cd6af91549baf1a69541f7b06
sathvi3 <sathvikbhandar007@hotmail.com>
2020-11-28 14:35:32 +0530

    added newGames and upcomingGames API.

--- src/actions/gamesAction.js
+++ src/actions/gamesAction.js
@@ -1,14 +1,18 @@
 import axios from "axios";
-import { popularGamesURL } from "../api";
+import { popularGamesURL, upcomingGamesURL, newGamesURL } from "../api";
 
 //Action Creator
 export const loadGames = () => async (dispatch) => {
   //FETCH AXIOS
   const popularData = await axios.get(popularGamesURL());
+  const newGamesData = await axios.get(newGamesURL());
+  const upcominData = await axios.get(upcomingGamesURL());
   dispatch({
     type: "FETCH_GAMES",
     payload: {
       popular: popularData.data.results,
+      upcoming: upcominData.data.results,
+      newGames: newGamesData.data.results,
     },
   });
 };
