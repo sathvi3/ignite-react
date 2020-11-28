d530e170a4a1e1da3d76d020046297d981e80e4a
sathvi3 <sathvikbhandar007@hotmail.com>
2020-11-28 14:45:13 +0530

    Refactored fetching Games in App to Home Page.

--- src/App.js
+++ src/App.js
@@ -1,15 +1,12 @@
-import React, { useEffect } from "react";
-import { useDispatch } from "react-redux";
-import { loadGames } from "./actions/gamesAction";
+import React from "react";
+//Components and Pages
+import Home from "./pages/Home";
 
 function App() {
-  const dispatch = useDispatch();
-  useEffect(() => {
-    dispatch(loadGames());
-  });
   return (
     <div className="App">
       <h1>Hello IGNITE🔥!!</h1>
+      <Home />
     </div>
   );
 }
