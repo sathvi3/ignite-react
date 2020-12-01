5d30446c11e6530dc69efed1ce9104cbd4d32347
sathvi3 <sathvikbhandar007@hotmail.com>
2020-12-01 13:39:30 +0530

    Routing support to the App.

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -8,8 +8,12 @@ import GameDetail from "../components/GameDetail";
 //Styling and Animation
 import styled from "styled-components";
 import { motion } from "framer-motion";
+import { useLocation } from "react-router-dom";
 
 const Home = () => {
+  //Get Current Location
+  const location = useLocation();
+  const pathId = location.pathname.split("/")[2];
   //FETCH GAMES
   const dispatch = useDispatch();
   useEffect(() => {
@@ -19,7 +23,7 @@ const Home = () => {
   const { popular, upcoming, newGames } = useSelector((state) => state.games);
   return (
     <GameList>
-      <GameDetail />
+      {pathId && <GameDetail />}
       <h2>Upcoming Games</h2>
       <Games>
         {upcoming.map((game) => (
