9722bf1019ea4f5585fbb1ab6c075ee668a1a122
sathvi3 <sathvikbhandar007@hotmail.com>
2020-12-01 16:09:59 +0530

    added Animation to Game and GameDetail Components.

--- src/components/Game.js
+++ src/components/Game.js
@@ -9,6 +9,7 @@ import { Link } from "react-router-dom";
 import { smallImage } from "../util";
 
 const Game = ({ name, released, image, id }) => {
+  const stringPathId = id.toString();
   //Load Details
   const dispatch = useDispatch();
   const loadDetailHandler = () => {
@@ -16,11 +17,15 @@ const Game = ({ name, released, image, id }) => {
     dispatch(loadDetail(id));
   };
   return (
-    <StyledGame onClick={loadDetailHandler}>
+    <StyledGame layoutId={stringPathId} onClick={loadDetailHandler}>
       <Link to={`/game/${id}`}>
-        <h3>{name}</h3>
+        <motion.h3 layoutId={`title ${stringPathId}`}>{name}</motion.h3>
         <p>{released}</p>
-        <img src={smallImage(image, 640)} alt={name} />
+        <motion.img
+          layoutId={`image ${stringPathId}`}
+          src={smallImage(image, 640)}
+          alt={name}
+        />
       </Link>
     </StyledGame>
   );
